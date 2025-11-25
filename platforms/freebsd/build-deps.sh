81e51c0720847a00e4903ae57f86a2dc2fe8af64
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-11-25 02:54:30 +0200

    Bump libdav1d to 1.5.2

--- platforms/freebsd/build-deps.sh
+++ platforms/freebsd/build-deps.sh
@@ -5,7 +5,7 @@ set -ex
 build_dav1d() {
   rm -rf "${BUILD_DIRECTORY}"/dav1d
   mkdir -p "${BUILD_DIRECTORY}"/dav1d
-  fetch -qo- https://download.videolan.org/videolan/dav1d/1.5.1/dav1d-1.5.1.tar.xz | tar xJ -C "${BUILD_DIRECTORY}/dav1d" --strip-components=1
+  fetch -qo- https://download.videolan.org/videolan/dav1d/1.5.2/dav1d-1.5.2.tar.xz | tar xJ -C "${BUILD_DIRECTORY}/dav1d" --strip-components=1
   pushd "${BUILD_DIRECTORY}"/dav1d
   meson setup build . "${DAV1D_MESON_ARGS[@]}"
   meson compile -C build --verbose
